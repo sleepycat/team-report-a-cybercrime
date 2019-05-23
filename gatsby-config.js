c5153104b659b307c7dca91bed80df0aab15c57c
Mike Williamson <mike.williamson@cds-snc.ca>
2019-05-23 17:17:10 -0400

    Build all the static things for GH pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/team-report-a-cybercrime",
   siteMetadata: {
     title: `Team "Report a cybercrime"`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
